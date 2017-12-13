26823083ac5c5b0941c3be7fef12f23a18b54254
Lucas Gertel <lgertel@gmail.com>
2017-12-13 17:35:56 -0200

    Non registrable for users

--- app/models/admin/user.rb
+++ app/models/admin/user.rb
@@ -2,6 +2,6 @@ class Admin::User < ApplicationRecord
   rolify :role_cname => 'Admin::Role'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
